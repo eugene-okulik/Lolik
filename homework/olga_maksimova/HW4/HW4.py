5a6f627246a7d60ca534020c3a9a2025d055a291
lolichka <123555195+lolik7447@users.noreply.github.com>
2024-08-25 16:10:51 +0300

    fix formatting issues

--- homework/olga_maksimova/HW4/HW4.py
+++ homework/olga_maksimova/HW4/HW4.py
@@ -1,13 +1,13 @@
-my_dict = {"tuple" : (1, 3, 6, 7, None, 'text', False, 2.42), 
-           "list" : [567, 'hello', 56.67, 'мир-труд-май', False] , 
-           "dict" : {'1':'Lenny Kravitz', '2f': 'Olya', '3,14568' : '5.66564', 'False' : 'True', 'пять': 'hfp ldf nhb'}, 
-           "set" : {1, 7, None, 'text', False, 2.42, 7}}
+my_dict = {"tuple": (1, 3, 6, 7, None, 'text', False, 2.42), 
+           "list": [567, 'hello', 56.67, 'мир-труд-май', False] , 
+           "dict": {'1':'Lenny Kravitz', '2f': 'Olya', '3,14568': '5.66564', 'False': 'True', 'пять': 'hfp ldf nhb'}, 
+           "set": {1, 7, None, 'text', False, 2.42, 7}}
 
 print(my_dict["tuple"][-1])
 my_dict["list"].append(42)
 my_dict["list"].pop(1)
 print(my_dict)
-my_dict["dict"] = {'i am a tuple':'good morning'}
+my_dict["dict"] = {'i am a tuple': 'good morning'}
 print(my_dict)
 my_dict['set'].add('руки вверх')
 print(my_dict)
