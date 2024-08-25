370e17da060bba48943348cc4fbb0faa9902239e
lolichka <123555195+lolik7447@users.noreply.github.com>
2024-08-25 16:25:06 +0300

    fix formatting issues v2

--- homework/olga_maksimova/HW4/HW4.py
+++ homework/olga_maksimova/HW4/HW4.py
@@ -1,6 +1,6 @@
-my_dict = {"tuple": (1, 3, 6, 7, None, 'text', False, 2.42), 
-           "list": [567, 'hello', 56.67, 'мир-труд-май', False] , 
-           "dict": {'1':'Lenny Kravitz', '2f': 'Olya', '3,14568': '5.66564', 'False': 'True', 'пять': 'hfp ldf nhb'}, 
+my_dict = {"tuple": (1, 3, 6, 7, None, 'text', False, 2.42),
+           "list": [567, 'hello', 56.67, 'мир-труд-май', False],
+           "dict": {'1': 'Lenny Kravitz', '2f': 'Olya', '3,14568': '5.66564', 'False': 'True', 'пять': 'hfp ldf nhb'},
            "set": {1, 7, None, 'text', False, 2.42, 7}}
 
 print(my_dict["tuple"][-1])
