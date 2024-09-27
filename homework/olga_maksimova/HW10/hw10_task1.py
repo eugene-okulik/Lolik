0e6eea044b3719d511129defd8c2a423969b0fea
Olga Maksimova <lolik7447@gmail.com>
2024-09-27 20:35:18 +0300

    fix errors

--- homework/olga_maksimova/HW10/hw10_task1.py
+++ homework/olga_maksimova/HW10/hw10_task1.py
@@ -2,12 +2,12 @@
 # он должен распечатывать слово "finished"после выполнения декорированной функции.
 
 def finish_me(func):
-
     def wrapper(*args):
         func(*args)
         print("finished")
 
     return wrapper
+
 
 @finish_me
 def example(text):
